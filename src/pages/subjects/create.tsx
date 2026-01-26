2dc8e9d6115a7f42a377ca95944abe5b81031e65
Muhammad Ain Uddin <muhammadainx@gmail.com>
2026-01-26 22:44:31 +0500

    initial commit

---
+++ src/pages/subjects/create.tsx
@@ -0,0 +1,5 @@
+const SubjectsCreate = () => {
+  return <div>Create Subjects</div>;
+};
+
+export default SubjectsCreate;
---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,5 @@
+const SubjectsList = () => {
+  return <div>List of Subjects</div>;
+};
+
+export default SubjectsList;
